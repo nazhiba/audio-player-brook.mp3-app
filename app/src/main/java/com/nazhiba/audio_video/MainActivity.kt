3df4263590a8a7b6f80a8d49665e3e256561c5b9
Nadzib <nadzib22@gmail.com>
2024-02-12 07:46:11 +0700

    aplikasi menjalankan audio brook.mp3

---
+++ app/src/main/java/com/nazhiba/audio_video/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.nazhiba.audio_video
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mediaPlayer = MediaPlayer.create(this, R.raw.brook)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(){
+            mediaPlayer.start()
+        }
+    }
+}
